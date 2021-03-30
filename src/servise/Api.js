ed146cf9eed6ad34f4cb7cc37288c9521632ea8b
Tatyana Loza <jes220@ukr.net>
2021-03-30 21:37:24 +0300

    currentPage  ошибка

--- src/servise/Api.js
+++ src/servise/Api.js
@@ -4,8 +4,8 @@ axios.defaults.baseURL = `https://pixabay.com/api/`;
 let myApiKey = `19822472-14ec0e3f9ba277805ee5b06f8`;
 const perPage = 12;
 
-const pixImages = ({ query = "", curentPage = 1 }) => {
-    const url = `https://pixabay.com/api/?q=${query}&page=${curentPage}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
+const pixImages = ({ query = "", currentPage = 1 }) => {
+    const url = `https://pixabay.com/api/?q=${query}&page=${currentPage}&key=${myApiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
 
     return axios.get(url).then(({ data }) => {
         return data.hits;
